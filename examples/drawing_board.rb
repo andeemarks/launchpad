b02b127de4d23d47b9bf10152a00619f1bb7a1d1
Andy Marks <vampwillow@gmail.com>
2017-08-10 21:41:17 +1000

    Updated for latest version of lib

--- examples/drawing_board.rb
+++ examples/drawing_board.rb
@@ -2,19 +2,14 @@ require 'launchpad'
 
 interaction = Launchpad::Interaction.new
 
-flags = Hash.new(false)
-
 # yellow feedback for grid buttons
 interaction.response_to(:grid, :down) do |interaction, action|
-  coord = 16 * action[:y] + action[:x]
-  brightness = flags[coord] ? :off : :hi
-  flags[coord] = !flags[coord]
-  interaction.device.change(:grid, action.merge(:red => brightness, :green => brightness))
+  interaction.device.change(:grid, :x => action[:x], :y => action[:y], :color => 13)
 end
 
 # mixer button terminates interaction on button up
 interaction.response_to(:mixer) do |interaction, action|
-  interaction.device.change(:mixer, :red => action[:state] == :down ? :hi : :off)
+  interaction.device.change(:mixer, :color => action[:state] == :down ? 5 : 61)
   interaction.stop if action[:state] == :up
 end
 
