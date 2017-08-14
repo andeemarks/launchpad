edba0aa27753a76bf333ab9865cd0822d410b45b
Andy Marks <vampwillow@gmail.com>
2017-08-14 10:30:23 +1000

    Added collision detection and control around game loop - not showing scores though

--- examples/pong.rb
+++ examples/pong.rb
@@ -6,6 +6,7 @@ UP = -1
 DOWN = 1
 
 @left_bottom_edge = @right_bottom_edge = 3
+@left_score = @right_score = 0
 
 def move_flipper(flipper, d, delta, column)
 	flipper = flipper + delta
@@ -63,26 +64,110 @@ interaction.response_to(:mixer, :up) do |interaction, action|
   interaction.stop
 end
 
+def hits_left_flipper?(new_ball_y)
+	new_ball_y >= @left_bottom_edge && new_ball_y <= @left_bottom_edge + 2
+end
+
+def hits_right_flipper?(new_ball_y)
+	new_ball_y >= @right_bottom_edge && new_ball_y <= @right_bottom_edge + 2
+end
+
+def update_scores(d)
+	puts "Left score: " + @left_score.to_s
+	puts "Right score: " + @right_score.to_s
+	if (@left_score > 0)
+		scene_button = (":scene" + @left_score.to_s).to_sym
+		puts scene_button
+		d.change(scene_button, :color => 72)
+	end
+	if (@right_score > 0)
+		scene_button = (":scene" + (9 - @right_score).to_s).to_sym
+		puts scene_button
+		d.change(scene_button, :color => 72)
+	end
+	if (@left_score == 4 || @right_score == 4)
+		@player_wins = true
+	end
+end
+
+def approaching_left_gutter?(new_ball_x)
+	new_ball_x < 1
+end
+
+def approaching_right_gutter?(new_ball_x)
+	new_ball_x > 6
+end
+
+def record_score(d)
+	update_scores(d)
+	hide_ball(d, @ball_x, @ball_y)
+end
+
+def record_right_score(d)
+	puts "Right score with ball at " + @ball_x.to_s + ", " + @ball_y.to_s + ", and left flipper at " + @left_bottom_edge.to_s
+	@right_score = @right_score + 1
+	record_score(d)
+end
+
+def record_left_score(d)
+	puts "Left score with ball at " + @ball_x.to_s + ", " + @ball_y.to_s + ", and right flipper at " + @right_bottom_edge.to_s
+	@left_score = @left_score + 1
+	record_score(d)
+end
+
+def bounce?(new_ball_y)
+	new_ball_y < 0 || new_ball_y > 7
+end
+
 def run_ball_movement(d) 
 	Thread.new {
-		loop do
-			sleep 0.5
-			hide_ball(d, @ball_x, @ball_y)
-			if ((@ball_x + @lat_delta) < 1 || (@ball_x + @lat_delta) > 6)
-				@lat_delta = -@lat_delta
+		@player_wins = false
+		until @player_wins do
+			new_score = false
+			start_ball(d)
+			until new_score do
+				sleep 0.5
+				if (approaching_left_gutter?(@ball_x + @lat_delta))
+					if (hits_left_flipper?(@ball_y + @lon_delta))
+						@lat_delta = -@lat_delta
+						move_ball(d, @ball_x + @lat_delta, @ball_y + @lon_delta)
+				else
+						record_right_score(d)
+						new_score = true
+					end
+				elsif (approaching_right_gutter?(@ball_x + @lat_delta))
+					if (hits_right_flipper?(@ball_y + @lon_delta))
+						@lat_delta = -@lat_delta
+						move_ball(d, @ball_x + @lat_delta, @ball_y + @lon_delta)
+					else
+						record_left_score(d)
+						new_score = true
+					end
+				end
+				if (!new_score)
+					if (bounce?(@ball_y + @lon_delta))
+						@lon_delta = -@lon_delta
+					end
+					move_ball(d, @ball_x + @lat_delta, @ball_y + @lon_delta)
+				end
 			end
-			@ball_x = @ball_x + @lat_delta
-			if ((@ball_y + @lon_delta) < 0 || (@ball_y + @lon_delta) > 7)
-				@lon_delta = -@lon_delta
-			end
-			@ball_y = @ball_y + @lon_delta
-			show_ball(d, @ball_x, @ball_y)
 		end
 	}
 end
 
+def move_ball(d, new_ball_x, new_ball_y)
+	hide_ball(d, @ball_x, @ball_y)
+	@ball_x = new_ball_x
+	@ball_y = new_ball_y
+	show_ball(d, @ball_x, @ball_y)
+end
+
 def show_ball(d, x, y)
 	d.change(:grid, :x => x, :y => y, :color => 72)	
+end
+
+def hilite_collision(d, x, y)
+	d.change(:grid, :x => x, :y => y, :color => 34)	
 end
 
 def hide_ball(d, x, y)
@@ -100,7 +185,6 @@ end
 
 show_flipper(interaction.device, 0, @left_bottom_edge)
 show_flipper(interaction.device, 7, @right_bottom_edge)
-start_ball(interaction.device)
 run_ball_movement(interaction.device)
 # start interacting
 interaction.start
